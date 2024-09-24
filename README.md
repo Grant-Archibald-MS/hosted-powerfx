668c07e3443a83e4b06650cfba956d084dacde6c
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2024-09-23 20:29:27 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# hosted-powerfx
-Sample repository that demonstrates how to host Power Fx in multiple locations
+# Hosted PowerFX Samples
+
+Sample repository that demonstrates how to host Power Fx in multiple languages
+
+## Getting started
+
+### Compile the .Net code
+
+1. Ensure NET 8.0 SDK is installed to create a .Net Standard 2.0 binary
+
+2. Change to PowerFx module
+
+```bash
+cd src/dotnet
+```
+
+3. Publish the example Power Fx class
+
+```bash
+dotnet publish -c Release  -r win-x64 --sc --framework netstandard2.0
+dotnet publish -c Release  -r win-x64 --sc --framework net8.0
+```
+
+### NodeJs
+
+To run the NodeJs sample
+
+1. Change to NodeJs source and install dependancies
+
+```
+cd src/nodejs
+npm install
+```
+
+2. Run the app
+
+```
+node main.js
+```
+
+### Python
+
+To run the Python sample
+
+1. Change to Python code and install dependancies
+
+```
+cd src/python
+pip install -r requirements.txt
+```
+
+5. Run the app
+
+```
+python main.py
+```
---
+++ src/nodejs/main.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const baseDll = path.join(__dirname, '../dotnet/bin/Release/net8.0/win-x64/publish/PowerFx.dll');
+
+const localTypeName = 'PowerFx.HostedPowerFx';
+process.env.EDGE_USE_CORECLR = 1;
+const edge = require('edge-js');
+
+const init = edge.func({
+    assemblyFile: baseDll,
+    typeName: localTypeName,
+    methodName: 'Init'
+});
+const evaluate = edge.func({
+    assemblyFile: baseDll,
+    typeName: localTypeName,
+    methodName: 'Evaluate'
+});
+
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+function initAsync() {
+    return new Promise((resolve, reject) => {
+        init(null, function(error, result) {
+            if (error) {
+                reject()
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+function evaluateAsync(input) {
+    return new Promise((resolve, reject) => {
+        evaluate(input, function(error, result) {
+            if (error) {
+                reject()
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+async function main() {
+    try {
+        await initAsync();
+        const rl = readline.createInterface({ input, output });
+
+        rl.on('line', async (input) => {
+            if (input === 'exit' || input === '\x1A') { // '\x1A' is Control Z
+                rl.close();
+                process.exit(0);
+            } else {
+                try {
+                    const result = await evaluateAsync(input);
+                    console.log(result);
+                    console.log('> ')
+                } catch (innerError) {
+                    console.error(innerError);
+                }
+            }
+        });
+
+        rl.setPrompt('> ');
+        rl.prompt();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
